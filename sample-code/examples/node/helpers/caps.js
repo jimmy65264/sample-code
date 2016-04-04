be609cc3e6da73d97f962df46c97efc3c6e99483
Shunzhe Ji <jimmy65264@gmail.com>
2016-04-04 17:57:04 -0400

    add scroll and swipe test

--- sample-code/examples/node/helpers/caps.js
+++ sample-code/examples/node/helpers/caps.js
@@ -3,8 +3,9 @@ exports.ios81 = {
   browserName: '',
   'appium-version': '1.3',
   platformName: 'iOS',
-  platformVersion: '8.1',
+  platformVersion: '9.1',
   deviceName: 'iPhone Simulator',
+  autoAcceptAlerts: true,
   app: undefined // will be set later
 };
 
--- sample-code/examples/node/ios-safari.js
+++ sample-code/examples/node/ios-safari.js
@@ -4,7 +4,10 @@ require("./helpers/setup");
 
 var wd = require("wd"),
     _ = require('underscore'),
-    serverConfigs = require('./helpers/appium-servers');
+    serverConfigs = require('./helpers/appium-servers'),
+    actions = require("./helpers/actions");
+
+wd.addPromiseChainMethod('swipe', actions.swipe);
 
 describe("ios safari", function () {
   this.timeout(300000);
@@ -27,13 +30,13 @@ describe("ios safari", function () {
   });
 
   after(function () {
-    return driver
-      .quit()
-      .finally(function () {
-        if (process.env.SAUCE) {
-          return driver.sauceJobStatus(allPassed);
-        }
-      });
+    // return driver
+    //   .quit()
+    //   .finally(function () {
+    //     if (process.env.SAUCE) {
+    //       return driver.sauceJobStatus(allPassed);
+    //     }
+    //   });
   });
 
   afterEach(function () {
@@ -43,34 +46,75 @@ describe("ios safari", function () {
 
   it("should get the url", function () {
     return driver
-      .get('https://www.google.com')
+      .get('https://devjobs.wirkn.com')
       .sleep(1000)
-      .waitForElementByName('q', 5000)
-        .sendKeys('sauce labs')
-        .sendKeys(wd.SPECIAL_KEYS.Return)
-      .sleep(1000)
-      .title().should.eventually.include('sauce labs');
+      // .waitForElementByName('q', 5000)
+      //   .sendKeys('sauce labs')
+      //   .sendKeys(wd.SPECIAL_KEYS.Return)
+      // .sleep(1000)
+      .title().should.eventually.equal('Wirkn Jobs');
   });
 
-  it("should delete cookie passing domain and path", function () {
-    var complexCookieDelete = function(name, path, domain) {
-      return function() {
-        path = path || '|';
-        return driver.setCookie({name: name, value: '', path: path, 
-          domain: domain, expiry: 0});        
-      };
-    };
-
+  it("should go to a job and go back to jobs list", function () {
+    var action = new wd.TouchAction();
+    action
+      .press({x: 0, y: 340})
+      .wait(800)
+      .moveTo({x: 220, y:340 })
+      .release();
     return driver
-      .get('http://en.wikipedia.org')
-      .waitForElementByCss('.mediawiki', 5000)
-      .allCookies() // 'GeoIP' cookie is there
-      .deleteCookie('GeoIP') 
-      .allCookies() // 'GeoIP' is still there, because it is set on
-                    // the .wikipedia.org domain
-      .then(complexCookieDelete('GeoIP', '/', '.wikipedia.org'))
-      .allCookies() // now 'GeoIP' cookie is gone
-      .sleep(1000);
-  });
+      .waitForElementByCss('#main-wrapper > div > div.col-md-8.no-padding-mobile > table > tbody > tr:nth-child(1)',5000)
+      .elementByCss('#main-wrapper > div > div.col-md-8.no-padding-mobile > table > tbody > tr:nth-child(1)', 500)
+      .click()
+      .sleep(1000)
+      .context("NATIVE_APP")
+      .performTouchAction(action);
+
+  })
+
+  it('should scroll down to view more jobs', function () {
+    var action = new wd.TouchAction();
+    action
+      .press({x: 187, y: 600})
+      .wait(800)
+      .moveTo({x: 187, y: 200})
+      .release();
+  // return this.performTouchAction(action)
+    return driver
+      .context("NATIVE_APP")
+      .performTouchAction(action)
+      .performTouchAction(action)
+      // .performTouchAction(action)
+      // .performTouchAction(action)
+      // .performTouchAction(action)
+      // .performTouchAction(action)
+      // .performTouchAction(action)
+      // .performTouchAction(action)
+      // .performTouchAction(action)
+      // .performTouchAction(action)
+      //.waitForElementByXpath("//*[@id=\"main-wrapper\"]/div/div[1]/table/tbody/tr[1]", 5000)
+      // .swipe({ startX: 50, startY: 0, endX: 50,  endY: 300, duration: 800 });
+  })
+
+  // it("should delete cookie passing domain and path", function () {
+  //   var complexCookieDelete = function(name, path, domain) {
+  //     return function() {
+  //       path = path || '|';
+  //       return driver.setCookie({name: name, value: '', path: path, 
+  //         domain: domain, expiry: 0});        
+  //     };
+  //   };
+
+  //   return driver
+  //     .get('http://en.wikipedia.org')
+  //     .waitForElementByCss('.mediawiki', 5000)
+  //     .allCookies() // 'GeoIP' cookie is there
+  //     .deleteCookie('GeoIP') 
+  //     .allCookies() // 'GeoIP' is still there, because it is set on
+  //                   // the .wikipedia.org domain
+  //     .then(complexCookieDelete('GeoIP', '/', '.wikipedia.org'))
+  //     .allCookies() // now 'GeoIP' cookie is gone
+  //     .sleep(1000);
+  // });
 
 });
